33da96d66b76c06903e3780d19e7e66c2686ab03
Gio Lodi <giovanni.lodi42@gmail.com>
2021-01-24 04:57:41 +1100

    Track project generated via Xcode template

---
+++ BuildSettingInjection/BuildSettingInjectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildSettingInjectionApp.swift
+//  BuildSettingInjection
+//
+//  Created by Gio on 24/1/21.
+//
+
+import SwiftUI
+
+@main
+struct BuildSettingInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
