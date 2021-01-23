9cb64a870cd52ffaa3bcc2f020130f7db3e6c758
Gio Lodi <giovanni.lodi42@gmail.com>
2021-01-24 05:08:17 +1100

    Add user-defined build setting CUSTOM_BUILD_SETTING
    The test still fails. What I'm trying to show is how to make it pass by
    passing a value to override the parameter when running the tests from
    the command line.

--- BuildSettingInjectionTests/BuildSettingInjectionTests.swift
+++ BuildSettingInjectionTests/BuildSettingInjectionTests.swift
@@ -8,6 +8,10 @@ class BuildSettingInjectionTests: XCTestCase {
         // Alternatively:
         // let bundle = Bundle.main
         let buildSetting = try XCTUnwrap(bundle.infoDictionary?["CUSTOM_BUILD_SETTING"] as? String)
+
+        // If run from Xcode, this test should fail with:
+        //
+        // > XCTAssertEqual failed: ("tomato") is not equal to ("bazinga")
         XCTAssertEqual(buildSetting, "bazinga")
     }
 }
